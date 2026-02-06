c16a62c93c308abc36d88dd2ba3914c4ffbf28e0
Luís Moreira <luismoreir@outlook.pt>
2026-02-06 15:32:19 +0000

    ...

---
+++ quadro6_4_2.py
@@ -0,0 +1,7 @@
+import numpy as np
+print ( 'Array bidimensional inicializado a zeros' )
+n_zeros=np.zeros(shape=(2,4),dtype= int )
+print (n_zeros)
+print ( 'Array unidimensional, criado e inicializado a zeros ' )
+n_zeros=np.zeros(shape=(3))
+print (n_zeros)
---
+++ quadro_6_3_5.py
@@ -0,0 +1,7 @@
+import numpy as np
+print ( 'Arr ay bidim e nsion al inicializado a uns' )
+n_ones=np.ones(shape=(3,2),dtype= int )
+print (n_ones)
+print ( 'Array unidimensional, criado e inicializado a un s ' )
+n_ones2=np.ones(shape=(4))
+print (n_ones2)
